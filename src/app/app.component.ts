89d8489f311b5c91b110fb146c840b2e83d2a92f
Sayan Halder <sayan.halder@iemlabs.com>
2024-03-15 19:27:26 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/modules/wishItems';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'HotelManagementNG';
+  items: WishItem[] = [
+    // thus we can load data into an HTML template
+    new WishItem('To Learn Angular', false),
+    new WishItem('Get Coffee', false),
+    new WishItem('Collect tickets', false),
+    new WishItem('Call Joe', false),
+  ];
+
+  newWishText: any = '';
+
+  title: string = 'Hotel XYZ';
+
+  visibleItems: WishItem[] = this.items;
+
+  listFilter: string = 'all';
+
+  addWishItem() {
+    this.items.push(new WishItem(this.newWishText, false));
+    this.newWishText = '';
+  }
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log('toggleItem event:', item);
+  }
+
+  filterChanged(value: string) {
+    console.log(this.listFilter);
+    if (value === 'all') {
+      this.visibleItems = this.items;
+    } else if (value === 'active') {
+      this.visibleItems = this.items.filter((item) => !item.isComplete);
+    } else if (value === 'fullfilled'){
+      this.visibleItems = this.items.filter((item) => item.isComplete);
+    }
+  }
 }
